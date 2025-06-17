828b322356040f613737c78ea463bdc310f768ea
soraya <soraya.asclepio@gmail.com>
2025-06-17 18:56:02 -0300

    primeiro commit do site

---
+++ js/script.js
@@ -0,0 +1,10 @@
+buttonEnviar.addEventListener("click", validaFormulario)
+
+function validaFormulario() {
+    if (document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("tel").value != "") {
+        alert("Prontinho! você receberá as novidades por email.")
+        form.reset();
+    } else {
+        alert("Por favor, preencha os campos nome, email e telefone!")
+    }
+}
